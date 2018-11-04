fe9474d91df4ba683b9e48e1aa00f71af5a81f4b
wherding <wherding@outlook.com>
2018-11-03 19:13:56 -0700

    first half of getting things set up. need to create an orm.js file

---
+++ db/seed.sql
@@ -0,0 +1,7 @@
+USE burgers_db;
+insert into burgers(burger_name, devoured)
+values ('will burger', 0);
+insert into burgers(burger_name, devoured)
+values ('burger', 0);
+insert into burgers(burger_name, devoured)
+values ('will not burger', 0); 
